a5fe9da9d6497ac59eef3c37bc45b032757c1c5a
nstanogias <nstanogias@gmail.com>
2022-05-28 20:19:55 +0200

    added track component

--- components/Body.tsx
+++ components/Body.tsx
@@ -2,6 +2,7 @@ import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import Poster from './Poster'
 import Search from './Search'
+import Track from './Track'
 
 const Body = ({ spotifyApi, chooseTrack }) => {
   const { data: session } = useSession()
@@ -85,6 +86,54 @@ const Body = ({ spotifyApi, chooseTrack }) => {
                   chooseTrack={chooseTrack}
                 />
               ))}
+      </div>
+      <div className="absolute flex min-w-full ml-6 gap-x-8 md:relative">
+        {/* Genres */}
+        <div className="hidden max-w-[270px] xl:inline">
+          <h2 className="mb-3 font-bold text-white">Genres</h2>
+          <div className="mb-3 flex flex-wrap gap-x-2 gap-y-2.5">
+            <div className="genre">Classic</div>
+            <div className="genre">House</div>
+            <div className="genre">Minimal</div>
+            <div className="genre">Hip-hop</div>
+            <div className="genre">Electronic</div>
+            <div className="genre">Chillout</div>
+            <div className="genre">Blues</div>
+            <div className="genre">Country</div>
+            <div className="genre">Techno</div>
+          </div>
+          <button className="w-full rounded-2xl bg-[#1A1A1A] bg-opacity-80 py-3.5 px-4 text-[13px] font-bold text-[#CECECE] transition ease-out hover:bg-opacity-100">
+            All Genres
+          </button>
+        </div>
+
+        {/* Tracks */}
+        <div className="w-full pr-11">
+          <h2 className="mb-3 font-bold text-white">
+            {searchResults.length === 0 ? 'New Releases' : 'Tracks'}
+          </h2>
+          <div className="scrollbar-thumb-rounded h-[1000px] w-[830px] space-y-3 overflow-y-scroll rounded-2xl border-2 border-[#262626] bg-[#0D0D0D] p-3 scrollbar-thin scrollbar-thumb-gray-600 hover:scrollbar-thumb-gray-500 md:h-96">
+            {searchResults.length === 0
+              ? newReleases
+                  .slice(4, newReleases.length)
+                  .map((track) => (
+                    <Track
+                      key={track.id}
+                      track={track}
+                      chooseTrack={chooseTrack}
+                    />
+                  ))
+              : searchResults
+                  .slice(4, searchResults.length)
+                  .map((track) => (
+                    <Track
+                      key={track.id}
+                      track={track}
+                      chooseTrack={chooseTrack}
+                    />
+                  ))}
+          </div>
+        </div>
       </div>
     </section>
   )
